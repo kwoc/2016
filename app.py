ceabb6266cb12a5c4a6f659ed9227475ab31cf8e
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2017-05-06 16:43:44 +0530

    Fix baisc index.html and leaderboard

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def student_register(request):
 						<table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
 								<tr>
 								<td bgcolor="#00081F" align="center" style="padding: 40px 0 30px 0;">
-										<img src="http://kwoc.kossiitkgp.in/static/img/kwoc_logo.png" width="300" height=auto style="display: block;" />
+										<img src="http://kwoc.kossiitkgp.instatic/img/kwoc_logo.png" width="300" height=auto style="display: block;" />
 								</td>
 								</tr>
 								<tr>
@@ -180,7 +180,7 @@ def project_register(request):
 						<table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
 								<tr>
 								<td bgcolor="#00081F" align="center" style="padding: 40px 0 30px 0;">
-										<img src="http://kwoc.kossiitkgp.in/static/img/kwoc_logo.png" width="300" height=auto style="display: block;" />
+										<img src="http://kwoc.kossiitkgp.instatic/img/kwoc_logo.png" width="300" height=auto style="display: block;" />
 								</td>
 								</tr>
 								<tr>
